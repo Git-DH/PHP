bfaf070d7ac80da66f4f294b54c6072c5e730f0b
Git-DH <kdh9345@naver.com>
2022-04-25 00:17:48 +0900

    php

--- board_login/join_proc.php
+++ board_login/join_proc.php
@@ -1,23 +1,24 @@
 <?php
     include_once "db/db_user.php";
 
-    $uid = $_POST["uid"];
-    $upw = $_POST["upw"];
-    $confirm_upw = $_POST["confirm_upw"];
-    $nm = $_POST["nm"];
-    $gender = $_POST["gender"];
+   $uid = $_POST['uid'];
+   $upw = $_POST['upw'];
+   $confirm_upw = $_POST['cofirm_upw'];
+   $nm = $_POST['nm'];
+   $gender = $_POST['gender'];
 
-    $param = [
-        "uid" => $uid,
-        "upw" => $upw,
-        "nm" => $nm,
-        "gender" => $gender
-    ];
+   $param = [
+    'uid' => $uid,
+    'upw' => $upw,
+    'confirm_upw' => $confirm_upw,
+    'nm' => $nm,
+    'gender' => $gender
+   ];
 
-    $result = user_join($param);
+   $result = user_join($param);
 
-    echo "uid : ", $uid, "<br>";
-    echo "upw : ", $upw, "<br>";
-    echo "confirm_upw : ", $confirm_upw, "<br>";
-    echo "nm : ", $nm, "<br>";
-    echo "gender : ", $gender, "<br>";
+   echo "uid", $uid, "<br>";
+   echo "upw", $upw, "<br>";
+   echo "confirm_upw", $confirm_upw, "<br>";
+   echo "nm", $nm, "<br>";
+   echo "gender", $gender, "<br>";
