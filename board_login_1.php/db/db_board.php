394b793cb8e1b9ab913d4f3e5e78a56c0b168292
Git-DH <kdh9345@naver.com>
2022-04-27 23:57:00 +0900

    php연습

---
+++ board_login_1.php/db/db_board.php
@@ -0,0 +1,21 @@
+<?php
+    include "db";
+
+    function ins_board(&$param) {
+        $i_user = $param["i_user"];
+        $title = $param["title"];
+        $ctnt = $param["ctnt"];
+
+        $conn = get_conn();
+        $sql = 
+        "INSERT INTO t_board
+        (i_user, title, ctnt)
+        VALUES
+        ($i_user, '$title', '$ctnt')
+        ";
+        $result = mysqli_query($conn, $sql);
+        mysqli_close($conn);
+        return $result;
+    }
+
+    
