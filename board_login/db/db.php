dbe69e818561acd5b8d02d6705620377fff1c422
Git-DH <kdh9345@naver.com>
2022-05-03 23:50:02 +0900

    php

--- board_login/db/db.php
+++ board_login/db/db.php
@@ -3,9 +3,8 @@
     define("USERNAME", "root");
     define("PASSWORD", "506greendg@");
     define("DB_NAME", "board_login");
-    
-    function get_conn() //적당한 중간 단계가 효율을 높인다
+
+    function get_conn() 
     {
         return mysqli_connect(URL, USERNAME, PASSWORD, DB_NAME);
     }
-?>
--- board_login_2/db/db_board.php
+++ board_login_2/db/db_board.php
@@ -17,3 +17,19 @@
         mysqli_close($conn);
         return $result;
     }
+
+    function sel_board_list()
+    {
+        $sql = 
+        "SELECT A.i_board, A.title, A.create_at, A.i_user, B.nm
+         FROM t_board A
+         INNER JOIN t_user B
+         ON A.i_user = B.i_user
+         ORDER BY A.i_board DESC
+        ";
+
+        $conn = get_conn();
+        $result = mysqli_query($conn, $sql);
+        mysqli_close($conn);
+        return $result;
+    }
